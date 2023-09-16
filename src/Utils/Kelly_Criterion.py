a0145d4127230facee08782dd23924ffbc959208
bcavs <cavenagh.ben@gmail.com>
2023-09-16 00:09:09 -0500

    fix kc

--- src/Utils/Kelly_Criterion.py
+++ src/Utils/Kelly_Criterion.py
@@ -2,16 +2,23 @@ def american_to_decimal(american_odds):
     """
     Converts American odds to decimal odds (European odds).
     """
-    if american_odds >= 100:
-        decimal_odds = (american_odds / 100)
+    if american_odds is not None:
+        if american_odds >= 100:
+            decimal_odds = american_odds / 100
+        else:
+            decimal_odds = 100 / abs(american_odds)
+        return round(decimal_odds, 2)
     else:
-        decimal_odds = (100 / abs(american_odds))
-    return round(decimal_odds, 2)
+        # Handle the case when american_odds is None
+        return None  # or raise an exception or return a default value
+
 
 def calculate_kelly_criterion(american_odds, model_prob):
     """
     Calculates the fraction of the bankroll to be wagered on each bet
     """
     decimal_odds = american_to_decimal(american_odds)
-    bankroll_fraction = round((100 * (decimal_odds * model_prob - (1 - model_prob))) / decimal_odds, 2)
+    bankroll_fraction = round(
+        (100 * (decimal_odds * model_prob - (1 - model_prob))) / decimal_odds, 2
+    )
     return bankroll_fraction if bankroll_fraction > 0 else 0
